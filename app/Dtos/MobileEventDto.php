39a83b530735d2c5316b1c716ba8ed79a29ffb3f
Alan <alan@MacBook-Pro-de-Alan.local>
2025-03-15 16:07:13 +0100

    refactor

--- app/Dtos/MobileEventDto.php
+++ app/Dtos/MobileEventDto.php
@@ -7,37 +7,30 @@ use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection; 
 use App\Models\Event;
 use App\State\MobileEventsStateProvider;
-use Illuminate\Support\Facades\Log;
-use ApiPlatform\Laravel\Eloquent\Filter\EqualsFilter;
 
 #[ApiResource(
     shortName: 'MobileEvent',
     operations: [
         new Get(provider: MobileEventsStateProvider::class),
-        new GetCollection(
-            provider: MobileEventsStateProvider::class,
-        ),
+        new GetCollection(provider: MobileEventsStateProvider::class),
     ],
     paginationEnabled: false
 )]
-
 class MobileEventDto
 {
     public function __construct(
         public int $id,
-        public String $title,
-        public ?String $startDate = null,
-        public ?String $endDate = null,
-        public ?String $address = '',
-        public ?String $status = '', // statuses(enum): accepted, declined, unknown, undefined, warning
-        public ?String $type = '', // type(enum):   training, performance, activity
-        public ?String $description = '',
+        public string $title,
+        public ?string $startDate = null,
+        public ?string $endDate = null,
+        public ?string $address = '',
+        public ?string $status = 'undefined', // statuses(enum): accepted, declined, unknown, undefined, warning
+        public ?string $type = '', // type(enum): training, performance, activity
+        public ?string $description = '',
         public ?int $companions = null,
-        //public List<TagModel>? $tags,
-        public ?String $comment = '',
-    ){}
+        public ?string $comment = '',
+    ) {}
 
-    // TODO: This method shouldn't be fromModel as it returns things thatdoesn't belong to the model, like attendance
     public static function fromModel(Event $event): MobileEventDto {
         $statusMap = [
             null => 'undefined',
@@ -45,16 +38,12 @@ class MobileEventDto
             2 => 'declined',
             3 => 'unknown',
         ];
-        $status = $statusMap[$event->status] ?? 'unknown';
 
         $typeMap = [
             1 => 'training',
             2 => 'performance',
             3 => 'activity',
         ];
-        $type = $typeMap[$event->type];
-
-        // Log::info("MobileEventDto_event_comments", [$event]);
 
         return new self(
             id: $event->id_event,
@@ -62,12 +51,10 @@ class MobileEventDto
             startDate: $event->start_date,
             endDate: $event->close_date,
             address: $event->address,
-            status: $status,
-            type: $type,
+            status: $statusMap[$event->status] ?? 'unknown',
+            type: $typeMap[$event->type] ?? '',
             description: $event->comments, // TODO: this should be a description
             companions: $event->companions,
-            //$event->tags,
-            //comment: $event->comment,
         );
     }
 }
--- app/State/AbstractStateProvider.php
+++ app/State/AbstractStateProvider.php
@@ -8,10 +8,8 @@ use ApiPlatform\Laravel\Eloquent\State\CollectionProvider;
 use ApiPlatform\Laravel\Eloquent\State\ItemProvider;
 use ApiPlatform\Metadata\CollectionOperationInterface;
 use ApiPlatform\Metadata\Operation;
-use ApiPlatform\State\ParameterNotFound;
 use ApiPlatform\State\ProviderInterface;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Log;
 use App\Models\ApiUser;
 
 abstract class AbstractStateProvider implements ProviderInterface
@@ -23,17 +21,14 @@ abstract class AbstractStateProvider implements ProviderInterface
         protected ItemProvider $itemProvider,
         protected CollectionProvider $collectionProvider
     ) {
-        Log::info('AbstractStateProvider._construct');
         try {
             // we get the authenticatedUserId by Token and then we retrieve the actual ApiUser
             if (!is_null($identifiedUserId = Auth::guard('sanctum')->id())) {
-                Log::info('AbstractStateProvider._construct.not_is_null');
                 $apiUser = ApiUser::find($identifiedUserId);
                 $this->casteller = $apiUser->getCastellerActive();
-                Log::info('AbstractStateProvider._construct.this_casteller', [$this->casteller]);
             }
         } catch (\Exception $e) {
-            Log::error('AbstractStateProvider._construct: ' . $e->getMessage());
+            // Handle exception if needed
         }
     }
 
@@ -56,13 +51,11 @@ abstract class AbstractStateProvider implements ProviderInterface
 
     protected function collectionProvider(Operation $operation, array $uriVariables = [], array $context = []): mixed
     {
-        Log::info('AbstractStateProvider.collectionProvider');
         return $this->collectionProvider->provide($operation, $uriVariables, $context);
     }
 
     protected function itemProvider(Operation $operation, array $uriVariables = [], array $context = []): mixed
     {
-        Log::info('AbstractStateProvider.itemProvider');
         return $this->itemProvider->provide($operation, $uriVariables, $context);
     }
 
