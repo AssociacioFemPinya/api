33fd8e1fec1b2bf78eb6bb36b0a096284d5db404
llorenllab <llorenllab@gmail.com>
2025-03-24 20:34:33 +0100

    Tags endpoint

--- app/State/TagsStateProvider.php
+++ app/State/TagsStateProvider.php
@@ -6,6 +6,7 @@ namespace App\State;
 
 use ApiPlatform\Metadata\Operation;
 use App\Models\Tag;
+use Illuminate\Support\Facades\Log;
 
 final class TagsStateProvider extends AbstractStateProvider
 {
@@ -15,12 +16,16 @@ final class TagsStateProvider extends AbstractStateProvider
         $tags = Tag::query();
 
         if (!is_null($this->casteller)) {
-            $tags->where('colla_id', $this->casteller->getColla());
+            $tags->where('colla_id', $this->colla->getId());
         }
-
-        if (array_key_exists('type', $this->parameters)) {
-            $tags->where('type', (string)$this->parameters['type']['value']);
-        }
+        
+        // Apply filters based on params
+        foreach ($this->parameters as $key => $value) {
+            match ($key) {
+                'type' => $tags->where('type', (string)$value),
+                default => null,
+            };
+        }  
 
         return $tags->get();
     }
